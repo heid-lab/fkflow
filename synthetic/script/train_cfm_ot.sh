04b92df9a9610b788c959327669160ea7e61e29e
Konstantin Mark <konstantin.mark@tuwien.ac.at>
2025-08-29 15:09:26 +0200

    Skip training additional guidance models.

--- synthetic/script/train_cfm_ot.sh
+++ synthetic/script/train_cfm_ot.sh
@@ -13,5 +13,4 @@ for pair in "circle s_curve" "8gaussian moon" "uniform 8gaussian"; do
     x0_dist=$1
     x1_dist=$2
     python guided_flow/train/cfm.py --x0_dist $x0_dist --x1_dist $x1_dist --cfm 'ot_cfm'
-    python guided_flow/train/value_guidance_matching.py --x0_dist $x0_dist --x1_dist $x1_dist
 done
