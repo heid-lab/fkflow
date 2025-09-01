33d90afa1862dd3dbf1d11ec6a65880b5550da96
hesther <esther_heid@web.de>
2025-09-01 11:20:34 +0200

    Update train_cfm_ot.sh

--- synthetic/script/train_cfm_ot.sh
+++ synthetic/script/train_cfm_ot.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-#SBATCH --partition=GPU-a40
-#SBATCH --gres=gpu:a40:1
-#SBATCH --nodes=1
-#SBATCH --job-name=ot_train
-#SBATCH --output=%x-%j.out
-
-cd /home/konstantin.mark/projects/flow_guidance/synthetic || exit
-
 for pair in "circle s_curve" "8gaussian moon" "uniform 8gaussian"; do
     set -- $pair
     x0_dist=$1
